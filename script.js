9b260e8dc1df3eb4aa4ad0283c57396a5107261e
David Matz <davidmatz@cs.uchicago.edu>
2020-09-21 15:12:57 -0500

    banned from t1 chat?

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function changeteam() {
+var t1 = ["1adrianaries1", "???", "shiphtur", "seanic", "starsmitten"];
+var t2 = ["???", "nightblue3", "strompest", "bukzachv2", "gweiss96"];
+var t3 = ["boxbox", "king_nidhogg", "pekinwoof", "xfsn_saber", "joeyyylol"];
+var t4 = ["generalsniperna", "loltyler1", "alwaysplanahealol", "duoking1", "tarzaned"];
+var t5 = ["nintendudex", "forestwithin", "tsmsword", "???", "lohpally"];
+var t6 = ["mistystumpey", "mikeyeung", "yassuo", "value247", "julienlol"];
+var t7 = ["dragoonl0l", "metaphor", "quantum", "???", "nasteeylol"];
+var t8 = ["tfblade", "warmachinehyper", "repobah", "wayofthetempest", "osama"];
+
+function changeteam1() {
+  //how to get argument here lol
+  var team = document.getElementById("team1").value;
+  alert(team)
   document.getElementById("a1").src = "https://www.twitch.tv/embed/tfblade/chat?parent=d-matz.github.io";
+  document.getElementById("a2").src = "https://www.twitch.tv/embed/tfblade/chat?parent=d-matz.github.io";
+  document.getElementById("a3").src = "https://www.twitch.tv/embed/tfblade/chat?parent=d-matz.github.io";
+  document.getElementById("a4").src = "https://www.twitch.tv/embed/tfblade/chat?parent=d-matz.github.io";
+  document.getElementById("a5").src = "https://www.twitch.tv/embed/tfblade/chat?parent=d-matz.github.io";
 }
